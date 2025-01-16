09c9907501ac3ebf89eeb7b191ce4279c98c8697
nurullo3313 <nurullo783313@gmail.com>
2025-01-16 10:32:13 +0300

    commit-nav

---
+++ src/components/Bot.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Bot() {
+    return (
+        <div className='fixed bottom-5 right-5 z-[999] 
+        cursor-pointer text-white text-4xl bg-cyan-600 w-16 h-16 flex 
+        items-center justify-center rounded-full sm:right-8 animate-bounce'>
+            <ion-icon name="chatbubble-ellipses"></ion-icon>
+        </div>
+    )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+        <div>
+            <div className='mx-7'>
+                <h4 className='text-4xl uppercase font-bold '>N<span className='text-cyan-600'>uru</span>llo</h4>
+            </div>
+        </div>
+    </nav>
+  )
+}
