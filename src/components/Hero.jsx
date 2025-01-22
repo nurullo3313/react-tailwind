bef650abc5dd271c00254442bd146dc67a9faed1
nurullo3313 <nurullo783313@gmail.com>
2025-01-22 10:32:28 +0300

    commit Home page-finish

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,20 +10,20 @@ export default function Hero() {
         <img src="./assets/myPhoto.png" alt="my photo" className='md:w-11/12 h-full object-cover'  />
       </div>
       <div className='flex-1'>
-        <div>
+        <div className='md:text-left text-center mt-10'>
           <h1 className='md:text-5xl text-2xl md:leading-normal leading-10 text-white font-bold'>
-            <span>
-              Hello
+            <span className='text-cyan-600 md:text-6xl text-5xl'>
+              Hello!!
               <br />
             </span>
             My name is <span className=''>Nurullo Abdukholiqov</span>
           </h1>
-          <h4>Frontend Developer</h4>
-          <button>Contact Me</button>
-          <div>
+          <h4 className='md:text-2xl text-lg md:leading-normal leading-5 mt-4 font-bold text-gray-600'>Frontend Developer</h4>
+          <button className='btn-primary mt-6'>Contact Me</button>
+          <div className='mt-8 text-3xl flex justify-center md:justify-start items-center gap-5'>
               {local_meadia.map((icon,i)=>(
-                <div key={i}>
-                  <ion-icon key={i} name={icon} class=""></ion-icon>
+                <div key={i} className='text-gray-600 hover:text-white cursor-pointer gap-5'>
+                  <ion-icon key={i} name={icon} ></ion-icon>
                 </div>
               ))}
           </div>
